e6cd713c96550911207fcec80754cffb599e189d
Marco <marcosilva@criticalmanufacturing.com>
2023-12-20 17:12:24 +0000

    feat: support from option

--- command.js
+++ command.js
@@ -103,6 +103,10 @@ const yargs = require('yargs')
     type: 'string',
     describe: 'Name of the package from which the tags will be extracted'
   })
+  .option('from', {
+    type: 'string',
+    describe: 'from'
+  })
   .check((argv) => {
     if (typeof argv.scripts !== 'object' || Array.isArray(argv.scripts)) {
       throw Error('scripts must be an object')
